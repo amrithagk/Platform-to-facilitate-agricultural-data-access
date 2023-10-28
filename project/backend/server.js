cb64d43b6ce97d07342eec75f30874dbccd8804d
ANEESH K B <kbaneesh2002@gmail.com>
2023-10-28 21:22:58 +0530

    login_singup_finish

--- project/backend/server.js
+++ project/backend/server.js
@@ -1,66 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { createClient } = require('@supabase/supabase-js');
+const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
-const port = 3000; // Your desired port
+const port = 5000; // Your desired port
 
-// Body parser middleware
+// Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
 
-const supabase = createClient(
-    process.env.PROJECT_URL,
-    process.env.API_KEY
-);
+// Import routes
+const fertilizersRoute = require('./routes/fertilizers');
+const loginRoute = require('./routes/login');
+const signupRoute = require('./routes/signup');
 
-app.post('/fertilizers', async (req, res) => {
-    const { name } = req.body; // Assuming the name is sent in the request body
-  
-    try {
-      const { data, error } = await supabase
-        .from('your_table_name')
-        .select('*')
-        .eq('name', name);
-  
-      if (error) {
-        return res.status(500).json({ error: 'Error searching the database.' });
-      }
-  
-      if (data && data.length > 0) {
-        return res.json(data);
-      } else {
-        return res.status(404).json({ message: 'No matching records found.' });
-      }
-    } catch (err) {
-      return res.status(500).json({ error: 'Server error.' });
-    }
-});
-
+// Use routes
+app.use('/fertilizers', fertilizersRoute);
+app.use('/login/', loginRoute);
+app.use('/signup/', signupRoute);
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
-/* export default async function handler(req, res) {
-    if (req.method === 'POST') {
-        const { name, email, message } = req.body;
-        try {
-            await supabase.from('Fertilizers').select([{ name: name, email: email, message: message }]);
-            return res.status(201).json({ message: "Message sent successfully" });
-        } catch (error) {
-            console.log("Error", error);
-            return res.status(500).json({ message: "There was an issue sending your message." })
-        }
-    } else {
-        return res.status(405).end()
-    }
-}
-
-const tableListener = supabase
-  .from('your_table_name')
-  .on('INSERT', (payload) => {
-    console.log('New row inserted:', payload.new);
-  })
-  .subscribe(); */
-
-  // Start the Express.js server
--- project/frontend/src/App.js
+++ project/frontend/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+// Import necessary modules
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+// Your components
+import Home from './pages/Home';
+import LoginSignUp from './pages/LoginSignUp';
+
+// Your main component (App.js or similar)
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        {/* Define your routes here */}
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginSignUp />} />
+      </Routes>
+    </Router>
   );
 }
 
