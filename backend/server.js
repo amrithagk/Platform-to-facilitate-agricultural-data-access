31c9aec27f6e289905ecc95991debdacebed96d0
Amritha <101631715+amrithagk@users.noreply.github.com>
2023-11-05 23:30:50 +0530

    dashboard first commit

--- backend/server.js
+++ backend/server.js
@@ -12,8 +12,8 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const supabase = createClient(
-    process.env.PROJECT_URL,
-    process.env.API_KEY
+  process.env.PROJECT_URL,
+  process.env.API_KEY
 );
 
 //console.log(supabase, "supa client")
@@ -22,56 +22,73 @@ app.post('/crops', async (req, res) => {
   const name = req.body.name; // Assuming the name is sent in the request body
   console.log(name)
   try {
-      //const user = supabase.auth.user();
-      const { data, error } = await supabase
-        .from('Crop')
-        .select('*')
-        .eq("Name", name);
-  
-      if (error) {
-        return res.status(500).json({ error: 'Error searching the database.' });
-      }
-  
-      if (data && data.length > 0) {
-        console.log("data", data)
-        return res.send(data);
-      } else {
-        return res.status(404).json({ message: 'No matching records found.' });
-      }
-    } catch (err) {
-      return res.status(500).json({ error: 'Server error.' });
+    //const user = supabase.auth.user();
+    const { data, error } = await supabase
+      .from('Crop')
+      .select('*')
+      .eq("Name", name);
+
+    if (error) {
+      return res.status(500).json({ error: 'Error searching the database.' });
     }
+
+    if (data && data.length > 0) {
+      console.log("data", data)
+      return res.send(data);
+    } else {
+      return res.status(404).json({ message: 'No matching records found.' });
+    }
+  } catch (err) {
+    return res.status(500).json({ error: 'Server error.' });
+  }
 });
 
 app.post('/fertilizers', async (req, res) => {
   const name = req.body.name; // Assuming the name is sent in the request body
   console.log(name)
   try {
-      //const user = supabase.auth.user();
-      const { data, error } = await supabase
+    //const user = supabase.auth.user();
+    const { data, error } = await supabase
       .from('Requires')
-  .select('*')
-  .filter('Crop.Scientific_Name', 'eq', 'Requires.Scientific_Name') // Condition for the join
-  .join('Crop', { type: 'inner' })
-  .eq('Name', name)
-      
-  
-      if (error) {
-        return res.status(500).json({ error: 'Error searching the database.' });
-      }
-  
-      if (data && data.length > 0) {
-        console.log("data", data)
-        return res.send(data);
-      } else {
-        return res.status(404).json({ message: 'No matching records found.' });
-      }
-    } catch (err) {
-      return res.status(500).json({ error: 'Server error.' });
+      .select('*')
+      .filter('Crop.Scientific_Name', 'eq', 'Requires.Scientific_Name') // Condition for the join
+      .join('Crop', { type: 'inner' })
+      .eq('Name', name)
+
+
+    if (error) {
+      return res.status(500).json({ error: 'Error searching the database.' });
     }
+
+    if (data && data.length > 0) {
+      console.log("data", data)
+      return res.send(data);
+    } else {
+      return res.status(404).json({ message: 'No matching records found.' });
+    }
+  } catch (err) {
+    return res.status(500).json({ error: 'Server error.' });
+  }
 });
+
+app.get('/dashboard', async (req, res) => {
+  const farmerID = req.body.farmerid;
+  try {
+    const { data, error } = await supabase
+      .from('Produce')
+      .select('*')
+      .filter('Farmer_id', 'eq', `${farmerID}`)
+    if (error) {
+      return res.status(500).json({ error: 'Error fetching from DB' })
+    } else {
+      return res.send(data)
+    }
+  } catch (err) {
+    return res.status(500).json({ error: "server error" })
+  }
+})
 
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
